a6339140557630611965201744ba7b480f38ff41
davidtsong <davidsongis@gmail.com>
2017-10-12 17:57:06 -0700

    Added initial code to work with firebase, added resty library, need to still parse data from json object and update into gui

---
+++ LeaderboardTester.java
@@ -0,0 +1,10 @@
+
+public class LeaderboardTester{
+    public static void main(String[] args)
+    {
+        Leaderboard leaderboard = new Leaderboard();
+//        leaderboard.ping();
+//        leaderboard.listEntries();
+        leaderboard.postEntry("Dad",100);
+    }
+}
